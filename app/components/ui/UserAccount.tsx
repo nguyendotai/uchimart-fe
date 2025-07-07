ca4eda12ac4d79d88af4677768d9d050e6f1bbd2
nguyendotai <nguyendotai11@gmail.com>
2025-07-07 14:08:04 +0700

    remove fake login

--- app/components/ui/UserAccount.tsx
+++ app/components/ui/UserAccount.tsx
@@ -6,46 +6,36 @@ import { User } from "@/app/types/User";
 import Link from "next/link";
 
 export default function UserAccount() {
-  const [user, setUser] = useState<User | null>(null);
-
-  const fakeLogin = () => {
-    setUser({
-      name: "Nguyễn Đỗ Tài",
-      email: "nguyendotai11@gmail.com",
-      avatar: "/1743665864604-483395787.jpg",
-    });
-  };
+  const [user, setUser] = useState<User | null>(null); // Sau này bạn thay bằng dữ liệu thực
 
   return (
-    <div
-      className="flex items-center gap-3 cursor-pointer w-[160px] overflow-hidden"
-      onClick={fakeLogin}
-    >
+    <div className="flex items-center gap-3 cursor-pointer w-[160px] overflow-hidden">
       {!user ? (
         <div className="flex items-center gap-2 text-gray-700 w-full h-10">
           {/* Icon trong nền tròn tím nhạt */}
           <div className="bg-[#f3e5f5] rounded-full p-2">
             <FaUser className="text-2xl shrink-0 text-[#921573]" />
           </div>
-
-          <Link href="/login"><span className="text-sm font-medium truncate">Đăng nhập</span></Link>
+          <Link href="/login">
+            <span className="text-sm font-medium truncate">Đăng nhập</span>
+          </Link>
         </div>
       ) : (
         <div className="flex items-center gap-3 w-full min-w-0">
           <div className="w-10 h-10 relative rounded-full overflow-hidden border border-gray-300 shrink-0">
             <Link href="/account">
-            <Image
-              src={user.avatar}
-              alt="User avatar"
-              fill
-              className="object-cover"
-            />
+              <Image
+                src={user.avatar}
+                alt="User avatar"
+                fill
+                className="object-cover"
+              />
             </Link>
           </div>
           <div className="flex flex-col overflow-hidden">
             <Link href="/account" className="flex flex-col overflow-hidden">
-            <span className="text-sm font-semibold truncate">{user.name}</span>
-            <span className="text-xs text-gray-500 truncate">{user.email}</span>
+              <span className="text-sm font-semibold truncate">{user.name}</span>
+              <span className="text-xs text-gray-500 truncate">{user.email}</span>
             </Link>
           </div>
         </div>
