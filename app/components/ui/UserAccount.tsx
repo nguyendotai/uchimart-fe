64d3833e01cec2d27c1f522537ecec0d265c481d
nguyendotai <nguyendotai11@gmail.com>
2025-07-07 15:15:08 +0700

    update icon account

--- app/components/ui/UserAccount.tsx
+++ app/components/ui/UserAccount.tsx
@@ -11,15 +11,16 @@ export default function UserAccount() {
   return (
     <div className="flex items-center gap-3 cursor-pointer w-[160px] overflow-hidden">
       {!user ? (
-        <div className="flex items-center gap-2 text-gray-700 w-full h-10">
+        <Link
+          href="/login"
+          className="flex items-center gap-2 text-gray-700 w-full h-10"
+        >
           {/* Icon trong nền tròn tím nhạt */}
           <div className="bg-[#f3e5f5] rounded-full p-2">
             <FaUser className="text-2xl shrink-0 text-[#921573]" />
           </div>
-          <Link href="/login">
-            <span className="text-sm font-medium truncate">Đăng nhập</span>
-          </Link>
-        </div>
+          <span className="text-sm font-medium truncate">Đăng nhập</span>
+        </Link>
       ) : (
         <div className="flex items-center gap-3 w-full min-w-0">
           <div className="w-10 h-10 relative rounded-full overflow-hidden border border-gray-300 shrink-0">
@@ -34,8 +35,12 @@ export default function UserAccount() {
           </div>
           <div className="flex flex-col overflow-hidden">
             <Link href="/account" className="flex flex-col overflow-hidden">
-              <span className="text-sm font-semibold truncate">{user.name}</span>
-              <span className="text-xs text-gray-500 truncate">{user.email}</span>
+              <span className="text-sm font-semibold truncate">
+                {user.name}
+              </span>
+              <span className="text-xs text-gray-500 truncate">
+                {user.email}
+              </span>
             </Link>
           </div>
         </div>
